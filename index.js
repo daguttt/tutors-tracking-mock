8d4a11da4ea09a0c6203088f0bffae50eb40709e
daguttt <dagutmu667@gmail.com>
2024-09-11 12:05:40 -0500

    wip feat: implement create a tutor tracking record for everty team leader and their assigned tutors

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const notionCl = new Client({
   auth: process.env.NOTION_KEY,
 });
 
-const ROOT_PAGE_ID = process.env.NOTION_PAGE_ID;
-const TUTORS_DB_ID = process.env.NOTION_TUTORS_DATABASE_ID;
+// const ROOT_PAGE_ID = process.env.NOTION_PAGE_ID;
+// const TUTORS_DB_ID = process.env.NOTION_TUTORS_DATABASE_ID;
+const TEAM_LEADERS_DB_ID = process.env.NOTION_TEAM_LEADERS_DATABASE_ID;
+const TUTORS_TRACKING_DB_ID = process.env.NOTION_TUTORS_TRACKING_DATABASE_ID;
 
 async function main() {
   // // Retrieve a specific page info
@@ -31,8 +33,23 @@ async function main() {
   // console.log();
 
   // Fetch non-archived Tutors DB
-  const tutorsDbQueryResponse = await notionCl.databases.query({
-    database_id: TUTORS_DB_ID,
+  // const tutorsDbQueryResponse = await notionCl.databases.query({
+  //   database_id: TUTORS_DB_ID,
+  //   filter: {
+  //     property: 'Archived',
+  //     checkbox: {
+  //       equals: false,
+  //     },
+  //   },
+  // });
+
+  // console.log();
+  // console.log(tutorsDbQueryResponse);
+  // console.log();
+
+  // Fetch non-archived Team Leaders DB
+  const teampLeadersDbQueryResponse = await notionCl.databases.query({
+    database_id: TEAM_LEADERS_DB_ID,
     filter: {
       property: 'Archived',
       checkbox: {
@@ -42,8 +59,62 @@ async function main() {
   });
 
   console.log();
-  console.log(tutorsDbQueryResponse);
+  console.log(teampLeadersDbQueryResponse);
   console.log();
+
+  // Create a tutor tracking record for each team leader and their assigned tutors
+  (async () => {
+    const teamLeaderPages = teampLeadersDbQueryResponse.results;
+    for (const teamLeadesPage of teamLeaderPages) {
+      const {
+        'Assigned Tutors': { relation: assignedTutorPageReferences },
+      } = teamLeadesPage.properties;
+
+      for (const assignedTutorPageReference of assignedTutorPageReferences) {
+        await createTutorTrackingPage({
+          teamLeaderPageId: teamLeadesPage.id,
+          tutorPageId: assignedTutorPageReference.id,
+          date: new Date(),
+        });
+      }
+    }
+  })();
 }
 
 main();
+
+/**
+ *
+ * @param {{teamLeaderPageId: string, tutorPageId: string, date: Date}} param0
+ */
+async function createTutorTrackingPage({
+  teamLeaderPageId,
+  tutorPageId,
+  date,
+}) {
+  await notionCl.pages.create({
+    parent: {
+      database_id: TUTORS_TRACKING_DB_ID,
+    },
+    properties: {
+      'Team Leader': {
+        type: 'relation',
+        relation: {
+          id: teamLeaderPageId,
+        },
+      },
+      Tutor: {
+        type: 'relation',
+        relation: {
+          id: tutorPageId,
+        },
+      },
+      Date: {
+        type: 'date',
+        date: {
+          start: date.toISOString(),
+        },
+      },
+    },
+  });
+}
---
+++ teamLeadersDbQueryResponseExample.js
@@ -0,0 +1,329 @@
+const teampLeadersDbQueryResponse = {
+  object: 'list',
+  results: [
+    {
+      object: 'page',
+      id: '28bed1ad-4bab-400f-92b3-a4fb9efbd166',
+      created_time: '2024-09-03T17:38:00.000Z',
+      last_edited_time: '2024-09-03T17:39:00.000Z',
+      created_by: {
+        object: 'user',
+        id: '49c1e069-a191-4536-abbd-3d6352d0a42e',
+      },
+      last_edited_by: {
+        object: 'user',
+        id: '49c1e069-a191-4536-abbd-3d6352d0a42e',
+      },
+      cover: null,
+      icon: {
+        type: 'external',
+        external: {
+          url: 'https://www.notion.so/icons/user_gray.svg',
+        },
+      },
+      parent: {
+        type: 'database_id',
+        database_id: '078089c2-586d-46f9-8c85-56df15c22ff6',
+      },
+      archived: false,
+      in_trash: false,
+      properties: {
+        'Tutor Days Tracked': {
+          id: '%3BXsL',
+          type: 'relation',
+          relation: [],
+          has_more: false,
+        },
+        Clan: {
+          id: 'LuUx',
+          type: 'relation',
+          relation: [
+            {
+              id: '399f652c-58ec-46bc-8af3-5bc5b73229a3',
+            },
+          ],
+          has_more: false,
+        },
+        'Assigned Tutors': {
+          id: 'VLf%3F',
+          type: 'relation',
+          relation: [
+            {
+              id: 'd7e0cc47-fa3e-41b6-9aef-37994ca3d799',
+            },
+          ],
+          has_more: false,
+        },
+        ID: {
+          id: 'Z%7CtI',
+          type: 'unique_id',
+          unique_id: {
+            prefix: 'TLD',
+            number: 4,
+          },
+        },
+        Archived: {
+          id: 'm%5D%5EL',
+          type: 'checkbox',
+          checkbox: false,
+        },
+        'Given Feedback to Tutors': {
+          id: 'p%3D%40%60',
+          type: 'relation',
+          relation: [],
+          has_more: false,
+        },
+        Name: {
+          id: 'title',
+          type: 'title',
+          title: [
+            {
+              type: 'text',
+              text: {
+                content: 'Milton Loaiza',
+                link: null,
+              },
+              annotations: {
+                bold: false,
+                italic: false,
+                strikethrough: false,
+                underline: false,
+                code: false,
+                color: 'default',
+              },
+              plain_text: 'Milton Loaiza',
+              href: null,
+            },
+          ],
+        },
+      },
+      url: 'https://www.notion.so/Milton-Loaiza-28bed1ad4bab400f92b3a4fb9efbd166',
+      public_url: null,
+    },
+    {
+      object: 'page',
+      id: '08beea08-a252-4e3e-907d-6f3efbc88cb9',
+      created_time: '2024-09-03T16:30:00.000Z',
+      last_edited_time: '2024-09-03T16:36:00.000Z',
+      created_by: {
+        object: 'user',
+        id: '91173e5e-b8b9-49b3-98ca-d76dbd03ece7',
+      },
+      last_edited_by: {
+        object: 'user',
+        id: '91173e5e-b8b9-49b3-98ca-d76dbd03ece7',
+      },
+      cover: null,
+      icon: {
+        type: 'external',
+        external: {
+          url: 'https://www.notion.so/icons/user_gray.svg',
+        },
+      },
+      parent: {
+        type: 'database_id',
+        database_id: '078089c2-586d-46f9-8c85-56df15c22ff6',
+      },
+      archived: false,
+      in_trash: false,
+      properties: {
+        'Tutor Days Tracked': {
+          id: '%3BXsL',
+          type: 'relation',
+          relation: [],
+          has_more: false,
+        },
+        Clan: {
+          id: 'LuUx',
+          type: 'relation',
+          relation: [
+            {
+              id: 'dbaf3f0d-18d3-4f8c-8512-68649f0fb3e1',
+            },
+          ],
+          has_more: false,
+        },
+        'Assigned Tutors': {
+          id: 'VLf%3F',
+          type: 'relation',
+          relation: [
+            {
+              id: '7c31bf7c-8e94-4759-a8bc-2c52077cb9a5',
+            },
+          ],
+          has_more: false,
+        },
+        ID: {
+          id: 'Z%7CtI',
+          type: 'unique_id',
+          unique_id: {
+            prefix: 'TLD',
+            number: 3,
+          },
+        },
+        Archived: {
+          id: 'm%5D%5EL',
+          type: 'checkbox',
+          checkbox: false,
+        },
+        'Given Feedback to Tutors': {
+          id: 'p%3D%40%60',
+          type: 'relation',
+          relation: [],
+          has_more: false,
+        },
+        Name: {
+          id: 'title',
+          type: 'title',
+          title: [
+            {
+              type: 'text',
+              text: {
+                content: 'Javier Cómbita Téllez',
+                link: null,
+              },
+              annotations: {
+                bold: false,
+                italic: false,
+                strikethrough: false,
+                underline: false,
+                code: false,
+                color: 'default',
+              },
+              plain_text: 'Javier Cómbita Téllez',
+              href: null,
+            },
+          ],
+        },
+      },
+      url: 'https://www.notion.so/Javier-C-mbita-T-llez-08beea08a2524e3e907d6f3efbc88cb9',
+      public_url: null,
+    },
+    {
+      object: 'page',
+      id: '89de195d-ffb8-485f-9e45-5d2190a61c75',
+      created_time: '2024-08-28T01:37:00.000Z',
+      last_edited_time: '2024-09-11T15:59:00.000Z',
+      created_by: {
+        object: 'user',
+        id: '91173e5e-b8b9-49b3-98ca-d76dbd03ece7',
+      },
+      last_edited_by: {
+        object: 'user',
+        id: '91173e5e-b8b9-49b3-98ca-d76dbd03ece7',
+      },
+      cover: null,
+      icon: {
+        type: 'external',
+        external: {
+          url: 'https://www.notion.so/icons/user_gray.svg',
+        },
+      },
+      parent: {
+        type: 'database_id',
+        database_id: '078089c2-586d-46f9-8c85-56df15c22ff6',
+      },
+      archived: false,
+      in_trash: false,
+      properties: {
+        'Tutor Days Tracked': {
+          id: '%3BXsL',
+          type: 'relation',
+          relation: [
+            {
+              id: 'd58e2996-6a04-4ae5-89b3-4989f38e7a5c',
+            },
+            {
+              id: 'ee555fd4-1ec2-4c60-a056-b9189a1aec02',
+            },
+            {
+              id: '07a247d8-297b-45cb-8044-2fda194f73ca',
+            },
+            {
+              id: '3a0f90e8-97eb-4755-8ee8-dde5afdd4165',
+            },
+            {
+              id: 'b794a339-f7b4-4f13-91b7-f14f7888d4b1',
+            },
+            {
+              id: 'daf8a726-c01b-4b0d-84fd-0ce47e6fe22b',
+            },
+            {
+              id: '65a543d5-f87b-4aac-b46f-532e9f1c76df',
+            },
+          ],
+          has_more: false,
+        },
+        Clan: {
+          id: 'LuUx',
+          type: 'relation',
+          relation: [
+            {
+              id: '50a86271-56cd-4e3c-85c5-b819d806186c',
+            },
+          ],
+          has_more: false,
+        },
+        'Assigned Tutors': {
+          id: 'VLf%3F',
+          type: 'relation',
+          relation: [
+            {
+              id: 'e920f90d-12e8-4d3c-81a0-3e3b6e5c8afe',
+            },
+          ],
+          has_more: false,
+        },
+        ID: {
+          id: 'Z%7CtI',
+          type: 'unique_id',
+          unique_id: {
+            prefix: 'TLD',
+            number: 2,
+          },
+        },
+        Archived: {
+          id: 'm%5D%5EL',
+          type: 'checkbox',
+          checkbox: false,
+        },
+        'Given Feedback to Tutors': {
+          id: 'p%3D%40%60',
+          type: 'relation',
+          relation: [],
+          has_more: false,
+        },
+        Name: {
+          id: 'title',
+          type: 'title',
+          title: [
+            {
+              type: 'text',
+              text: {
+                content: 'Antony Martinez Ruiz',
+                link: null,
+              },
+              annotations: {
+                bold: false,
+                italic: false,
+                strikethrough: false,
+                underline: false,
+                code: false,
+                color: 'default',
+              },
+              plain_text: 'Antony Martinez Ruiz',
+              href: null,
+            },
+          ],
+        },
+      },
+      url: 'https://www.notion.so/Antony-Martinez-Ruiz-89de195dffb8485f9e455d2190a61c75',
+      public_url: null,
+    },
+  ],
+  next_cursor: null,
+  has_more: false,
+  type: 'page_or_database',
+  page_or_database: {},
+  request_id: '2c102918-6703-495c-8a95-fa77f17e8e46',
+};
